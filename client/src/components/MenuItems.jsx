e79bff2eac639a5613321ababcc65fb9ab983f5b
omrajkamat68 <ob2614@srmist.edu.in>
2025-08-24 10:25:10 +0530

    Sidebar component completed.

--- client/src/components/MenuItems.jsx
+++ client/src/components/MenuItems.jsx
@@ -5,7 +5,7 @@ import { NavLink } from "react-router-dom";
 const MenuItems = ({ setSidebarOpen }) => {
   return (
     <div className="px-6 text-gray-600 space-y-1 font-medium">
-      {menuItemsData.map((to, label, Icon) => (
+      {menuItemsData.map(({to, label, Icon}) => (
         <NavLink
           key={to}
           to={to}
