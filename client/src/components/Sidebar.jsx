d4861b81a31db2089eae86733f4f3a73733746f6
omrajkamat68 <ob2614@srmist.edu.in>
2025-08-24 09:42:46 +0530

    Menuitems component added.

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import {useNavigate} from 'react-router-dom'
+import { assets } from '../assets/assets'
 
-const Sidebar = () => {
+const Sidebar = ({sidebarOpen, setSidebarOpen}) => {
+
+  const navigate = useNavigate()
+
   return (
-    <div>
-      <h1>This is SIdebar</h1>
+    <div className={`w-60 xl:w-72 bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute top-0 bottom-0 z-20 ${sidebarOpen ? 'translate-x-0' : 'max-sm:-translate-x-full'} transition-all duration-300 ease-in-out`}>
+      <div className='w-full'>
+        <img onClick={()=> navigate('/')} src={assets.logo} className='w-26 ml-7 my-2 cursor-pointer' alt="" />
+        <hr className='border-gray-300 mb-8' />
+      </div>
     </div>
   )
 }
