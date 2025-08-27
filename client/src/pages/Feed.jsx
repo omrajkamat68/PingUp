b7c6bab32e1fc2126b2f4f1cbfd964530bd0b7fd
omrajkamat68 <ob2614@srmist.edu.in>
2025-08-27 19:03:22 +0530

    Post card component added.

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { dummyPostsData } from '../assets/assets'
 import Loading from '../components/Loading'
 import StoriesBar from '../components/StoriesBar'
+import PostCard from '../components/PostCard'
 
 const Feed = () => {
 
@@ -23,7 +24,9 @@ const Feed = () => {
       <div>
         <StoriesBar />
         <div className='p-4 space-y-6'>
-          Lidisfhibv
+          {feeds.map((post)=>(
+            <PostCard key={post._id} post={post} />
+          ))}
         </div>
       </div>
 
@@ -32,7 +35,7 @@ const Feed = () => {
         <div>
           <h1>Sponsored</h1>
         </div>
-        <h1>Rsndbvjfv</h1>
+        <h1>Recent Messages</h1>
       </div>
     </div>
   ) : <Loading />
