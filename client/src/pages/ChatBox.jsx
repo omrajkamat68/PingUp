f7e96ada4f36eb88a04fe5f6c58c14814784e491
omrajkamat68 <ob2614@srmist.edu.in>
2025-09-01 19:11:04 +0530

    Chat box added.

--- client/src/pages/ChatBox.jsx
+++ client/src/pages/ChatBox.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { dummyMessagesData, dummyUserData } from '../assets/assets'
 
 const ChatBox = () => {
+
+  const messages = dummyMessagesData
+  const [text, setText] = useState('')
+  const [image, setImage] = useState(null)
+  const [user, setUser] = useState(dummyUserData)
+  const messagesEndRef = useRef(null)
+
+  const sendMessage = async () => {
+    
+  }
+
+  useEffect(()=>{
+    messagesEndRef.current?.scrollIntoView({behavior: "smooth"})
+  },[messages])
+
   return (
-    <div>
-      
+    <div className='flex flex-col h-screen'>
+      <div className='flex items-center gap-2 p-2 md:px-10 xl:pl-42 bg-gradient-to-r from-indigo-50 to-purple-50 border-b border-gray-300'>
+        <img src={user.profile_picture} alt="" className='size-8 rounded-full' />
+        <div>
+          <p className='font-medium'>{user.full_name}</p>
+          <p className='text-sm text-gray-500 -mt-1.5'>@{user.username}</p>
+        </div>
+      </div>
+      <div className='p-5 md:px-10 h-full overflow-y-scroll'>
+        <div className='space-y-4 max-w-4xl mx-auto'>
+          {
+            messages.toSorted((a,b)=> new Date(a.createdAt) - new Date(b.createdAt)).map((message, index)=>(
+              <div key={index} className={`flex flex-col ${message.to_user_id !== user._id ? 'items-start' : 'items-end'}`}>
+                <div className={`p-2 text-sm max-w-sm bg-white text-slate-700 rounded-lg shadow ${message.to_user_id !== user._id ? 'rounded-bl-none' : 'rounded-br-none'}`}>
+                  {
+                    message.message_type === 'image' && <img src={message.media_url} className='w-full max-w-sm rounded-lg mb-1' alt="" />
+                  }
+                  <p>{message.text}</p>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
     </div>
   )
 }
