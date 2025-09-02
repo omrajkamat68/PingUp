90e53526deaa80ad03c3258e82d3792584538188
omrajkamat68 <ob2614@srmist.edu.in>
2025-09-02 14:04:11 +0530

    Basic Express app created.

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => res.send("Server is running"));
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
