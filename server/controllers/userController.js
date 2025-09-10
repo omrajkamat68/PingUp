34173135a62ed7723fdba6108bb88649bb17785b
omrajkamat68 <ob2614@srmist.edu.in>
2025-09-10 09:47:51 +0530

    Controller function to Get user profiles and posts added.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,6 @@
 import imagekit from "../configs/imageKit.js";
 import Connection from "../models/Connection.js";
+import Post from "../models/Post.js";
 import User from "../models/User.js";
 import fs from 'fs'
 
@@ -251,6 +252,23 @@ export const acceptConnectionRequest = async (req, res) => {
 
         res.json({success: true, message: 'Connection accepted successfully.'})
 
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message})
+    }
+}
+
+// Get user profiles
+export const getUserProfiles = async (req, res) => {
+    try {
+        const {profileId} = req.body
+        const profile = await User.findById(profileId)
+        if(!profile){
+            return res.json({ success: false, message: "Profile not found" })
+        }
+        const posts = await Post.find({user: profileId}).populate('user')
+        
+        res.json({success: true, profile, posts})
     } catch (error) {
         console.log(error);
         res.json({success: false, message: error.message})
