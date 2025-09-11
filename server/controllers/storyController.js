26229abc95d987f60b401ba77bc37c7b3647c549
omrajkamat68 <ob2614@srmist.edu.in>
2025-09-11 09:32:40 +0530

    Inngest function to delete story after 24 hours added.

--- server/controllers/storyController.js
+++ server/controllers/storyController.js
@@ -2,6 +2,7 @@ import fs from "fs"
 import imagekit from "../configs/imageKit.js"
 import Story from "../models/Story.js"
 import User from "../models/User.js"
+import { inngest } from "../inngest/index.js"
 
 // Add User Story
 export const addUserStory = async (req, res) => {
@@ -12,7 +13,7 @@ export const addUserStory = async (req, res) => {
         let media_url = ''
 
         // upload media to imagekit
-        if(media_type == 'image' || media_type == 'video'){
+        if(media_type === 'image' || media_type === 'video'){
             const fileBuffer = fs.readFileSync(media.path)
             const response = await imagekit.upload({
                 file: fileBuffer,
@@ -27,6 +28,12 @@ export const addUserStory = async (req, res) => {
             media_url,
             media_type,
             background_color
+        })
+
+        // schedule story deletion after 24 hours
+        await inngest.send({
+            name: 'app/story.delete',
+            data: {storyId: story._id}
         })
 
         res.json({success: true})
