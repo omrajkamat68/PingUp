5f5f987cb9ad50ba3da94dbba748831828ed0ab9
omrajkamat68 <ob2614@srmist.edu.in>
2025-09-12 09:37:22 +0530

    Controller function to Send Message added.

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,3 +1,7 @@
+import fs from 'fs'
+import imagekit from '../configs/imageKit.js';
+import Message from '../models/Message.js';
+
 // Create an empty object to store SS event connections
 const connections = {}
 
@@ -24,4 +28,54 @@ export const sseController = (req, res)=>{
         delete connections[userId]
         console.log('Client disconnected');
     })
+}
+
+// Send Message
+export const sendMessage = async (req, res) => {
+    try {
+        const {userId} = req.auth()
+        const {to_user_id, text} = req.body
+        const image = req.file
+
+        let media_url = ''
+        let message_type = image ? 'image' : 'text'
+
+        if(message_type === 'image'){
+            const fileBuffer = fs.readFileSync(image.path)
+            const response = await imagekit.upload({
+                file: fileBuffer,
+                fileName: image.originalname,
+            });
+            media_url = imagekit.url({
+                path: response.filePath,
+                transformation: [
+                    {quality: 'auto'},
+                    {format: 'webp'},
+                    {width: '1280'}
+                ]
+            })
+        }
+
+        const message = await Message.create({
+            from_user_id: userId,
+            to_user_id,
+            text,
+            message_type,
+            media_url
+        })
+
+        res.json({ success: true, message })
+
+        // Send message to to_user_id using SSE
+
+        const messageWithUserData = await Message.findById(message._id).populate('from_user_id')
+
+        if(connections[to_user_id]){
+            connections[to_user_id].write(`data: ${JSON.stringify(messageWithUserData)}\n\n`)
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
 }
