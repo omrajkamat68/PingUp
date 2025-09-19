b5d53ce7b34215994c53891fcfcfdc8ce7ed4493
omrajkamat68 <ob2614@srmist.edu.in>
2025-09-19 09:36:09 +0530

    Connection slice updated.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import {Toaster} from 'react-hot-toast'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { fetchUser } from './features/user/userSlice'
+import { fetchConnections } from './features/connections/connectionsSlice'
 
 const App = () => {
   const {user} = useUser()
@@ -26,6 +27,7 @@ const App = () => {
       if(user){
       const token = await getToken()
       dispatch(fetchUser(token))
+      dispatch(fetchConnections(token))
       }
     }
     fetchData()
