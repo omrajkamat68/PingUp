d2956434a058a63e9256be48b063b362ad41c1b2
omrajkamat68 <ob2614@srmist.edu.in>
2025-09-20 13:14:38 +0530

    Error fixed in user controller.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -185,8 +185,8 @@ export const sendConnectionRequest = async (req, res) => {
         // Check if users are already connected
         const connection = await Connection.findOne({
             $or: [
-                {rom_user_id: userId, to_user_id: id},
-                {rom_user_id: id, to_user_id: userId}
+                {from_user_id: userId, to_user_id: id},
+                {from_user_id: id, to_user_id: userId}
             ]
         })
 
